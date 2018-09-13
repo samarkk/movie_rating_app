ff8886994e7ee22c62bef05d02c8dc08bb6c03f4
samarkk <samarkant1@yahoo.com>
2018-09-13 14:35:34 +0530

    movie_profile_working

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,20 @@
 const MovieSchema = require('../models/Movie.js')
 
 module.exports.controller = (app) => {
+	app.get('/movies', (req, res) => {
+		MovieSchema.find({}, 'name description release_year genre', (error, movies) => {
+			if(error) { console.log(error) }
+			res.send({
+				movies
+			})
+		})
+	})
+	app.get('/api/movies/:id', (req, res) => {
+		MovieSchema.findById(req.params.id, 'name description release_year genre', (error, movie) => {
+		  if(error) { console.log(error) }
+		  res.send(movie)
+		})
+	})
 	app.post('/movies', (req, res) => {
 		const newMovie = new MovieSchema({
 			name: req.body.name,
