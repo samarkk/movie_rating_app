d45729e5d8d198be8f122552313762ff4cb00265
samarkk <samarkant1@yahoo.com>
2018-09-13 10:55:47 +0530

    server_controller_working

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -9,6 +9,7 @@
     </v-form>
 </template>
 <script>
+import axios from 'axios'
 export default {
   data: () => ({
     valid: true,
@@ -30,8 +31,27 @@ export default {
   methods: {
     submit () {
       if (this.$refs.form.validate()) {
-        // Perform next action
+        return axios({
+          method: 'post',
+          data: {
+            name: this.name,
+            description: this.description,
+            release_year: this.release_year,
+            genre: this.genre
+          },
+          url: 'http://localhost:8081/movies',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(() => {
+            this.$router.push({name: 'Home'})
+            this.$refs.form.reset()
+          })
+          .catch(() => {
+          })
       }
+      return true
     },
     clear () {
       this.$refs.form.reset()
