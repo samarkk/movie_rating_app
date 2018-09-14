4e80afdbb453afbdb0acc747ddeafe3535277c1b
samarkk <samarkant1@yahoo.com>
2018-09-14 09:43:53 +0530

    user_registration_working_ch_6

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,9 +1,9 @@
-const MovieSchema = require('../models/Movie.js')
+const Movie = require('../models/Movie.js')
 const Rating = require('../models/Rating.js')
 
 module.exports.controller = (app) => {
 	app.get('/movies', (req, res) => {
-		MovieSchema.find({}, 'name description release_year genre', (error, movies) => {
+		Movie.find({}, 'name description release_year genre', (error, movies) => {
 			if(error) { console.log(error) }
 			res.send({
 				movies
@@ -11,13 +11,13 @@ module.exports.controller = (app) => {
 		})
 	})
 	app.get('/api/movies/:id', (req, res) => {
-		MovieSchema.findById(req.params.id, 'name description release_year genre', (error, movie) => {
+		Movie.findById(req.params.id, 'name description release_year genre', (error, movie) => {
 		  if(error) { console.log(error) }
 		  res.send(movie)
 		})
 	})
 	app.post('/movies', (req, res) => {
-		const newMovie = new MovieSchema({
+		const newMovie = new Movie({
 			name: req.body.name,
 			description: req.body.description,
 			release_year: req.body.release_year,
